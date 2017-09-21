61a85d0ecb71994a792de2a747b6b7ef7265e6d8
Pando85 <pando855@gmail.com>
2017-09-21 23:11:34 +0200

    Fix basic model

--- tortoise/main/models/task.py
+++ tortoise/main/models/task.py
@@ -9,12 +9,14 @@ class Task(models.Model):
     title = models.CharField(max_length=80)
     description = models.CharField(max_length=500, blank=True)
     creation_date = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateTimeField(blank=True)
-    owner = models.ForeignKey(User)
-    usersgroup = models.ManyToManyField(User)
+    deadline = models.DateTimeField(blank=True, null=True)
+    owner = models.ForeignKey(User, related_name='tasks_ownership')
+    members = models.ManyToManyField(
+        User, related_name='tasks_membership', blank=True)
     tags = models.ManyToManyField(Tag, blank=True)
 
     def clean(self):
-        if self.deadline <= self.creation_date:
-            raise ValidationError(
-                _('Deadline specified is setted before creation date'))
+        if self.deadline:
+            if self.deadline <= self.creation_date:
+                raise ValidationError(
+                    'Deadline specified is setted before creation date')
--- tortoise/main/models/user.py
+++ tortoise/main/models/user.py
@@ -1,15 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 
-from .managers import UserManager
 
-
-class User(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser):
     username = models.CharField(max_length=32, unique=True)
     email = models.EmailField(unique=True)
     date_joined = models.DateTimeField(auto_now_add=True)
-    objects = UserManager()
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['username', 'email']
