5af49a95ff1c050a0f206bde8ffa8b96a30c317f
Sandra Saez <ssaez@datiobd.com>
2017-10-01 13:07:19 +0200

    Views implemented using ModelViewSet approach

---
+++ tortoise/main/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
+from api import TagSerializer, TaskSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+
