43617768b2ddbe1af50b84e502a15a65fa152acd
Pando85 <pando855@gmail.com>
2018-02-04 21:47:04 +0100

    Add prety print for tasks

---
+++ telegram_bot/tortoise_bot/commands/tools.py
@@ -0,0 +1,29 @@
+import shlex
+
+
+def parse_match(match, parser):
+    args_list = shlex.split(match)
+    try:
+        args = parser.parse_args(args_list)
+    except SystemExit:
+        args = False
+    return args
+
+
+def beauty_tasks(tasks):
+    output = ''
+    for task in tasks:
+        output += f"- *{task['name']}*\n"
+        if task['description']:
+            output += f"   `{task['description']}`\n"
+
+        if task['deadline']:
+            output += f"   Limit:{task['deadline']}\n"
+
+        if task['tags']:
+            output += f"    tags:{task['tags']}\n"
+
+        if task['members']:
+            output += f"    tags:{task['members']}\n"
+        output += "\n"
+    return output
