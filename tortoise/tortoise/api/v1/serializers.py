d3aa9666d5683b2fe637bf13d75117f858e92158
Pando85 <pando855@gmail.com>
2018-02-18 22:54:02 +0100

    Add create tags from posts

--- tortoise/tortoise/api/v1/serializers.py
+++ tortoise/tortoise/api/v1/serializers.py
@@ -1,8 +1,23 @@
+from django.utils.encoding import smart_text
+from django.core.exceptions import ObjectDoesNotExist
+
 from rest_framework import serializers
 
 from tortoise.main.models.user import User
 from tortoise.main.models.tag import Tag
 from tortoise.main.models.task import Task
+
+
+class CreatableSlugRelatedField(serializers.SlugRelatedField):
+    def to_internal_value(self, data):
+        try:
+            return self.get_queryset().get_or_create(
+                **{self.slug_field: data})[0]
+        except ObjectDoesNotExist:
+            self.fail('does_not_exist',
+                      slug_name=self.slug_field, value=smart_text(data))
+        except (TypeError, ValueError):
+            self.fail('invalid')
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -12,6 +27,11 @@ class TagSerializer(serializers.ModelSerializer):
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    tags = CreatableSlugRelatedField(
+        queryset=Tag.objects.all(),
+        many=True,
+        slug_field='name')
+
     class Meta:
         model = Task
         fields = ('name', 'description', 'deadline', 'members', 'tags')
